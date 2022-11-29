24d168310c595ac2e9ae326d0ae70498f7045372
孙宗文 <sunzongwen@sunzongwendeMacBook-Pro.local>
2022-11-29 21:51:52 +0800

    NEW: create android project

---
+++ android/app/src/main/java/com/szw/fresh/MainActivity.java
@@ -0,0 +1,30 @@
+package com.szw.fresh;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.szw.fresh.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+}
